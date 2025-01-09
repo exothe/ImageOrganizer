e47204981ce46fc9deac11ecc807b258b23e7e2a
MissSis <julius.bmw@gmx.de>
2025-01-09 14:51:12 +0100

    add bump_version script

---
+++ bump-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+action=$1
+
+if [ "$action" != "patch" ] && [ "$action" != "minor" ] && [ "$action" != "major" ]; then
+    echo 'You have to specify how you want to bump the version, i.e.'
+    echo
+    printf "\tbump-version.sh patch|minor|major\n"
+    exit
+fi
+
+new_version=$(npm version $action --git-tag-version=false)
+cd src-tauri
+cargo release version -x --no-confirm -q $action
+jq --arg version "${new_version:1}" '.version |= $version' tauri.conf.json | sponge tauri.conf.json
+git commit -am "$new_version"
