3abdfa0835328cbefead9986606b345f8eb90a7a
exothe <julius.bmw@gmx.de>
2025-12-06 13:35:02 +0100

    incorporate bump-version in release action

--- bump-version.sh
+++ bump-version.sh
@@ -13,4 +13,8 @@ new_version=$(npm version $action --git-tag-version=false)
 cd src-tauri
 cargo release version -x --no-confirm -q $action
 jq --arg version "${new_version:1}" '.version |= $version' tauri.conf.json | sponge tauri.conf.json
+npx --yes prettier --write tauri.conf.json
 git commit -am "$new_version"
+git tag $new_version
+git push
+git push origin $new_version
