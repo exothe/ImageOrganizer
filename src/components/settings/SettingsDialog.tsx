89d5964f52febd563d775750222be5721502fb7a
exothe <julius.bmw@gmx.de>
2025-12-06 22:51:46 +0100

    show app version in settings dialog

--- src/components/settings/SettingsDialog.tsx
+++ src/components/settings/SettingsDialog.tsx
@@ -3,15 +3,42 @@ import React from 'react';
 import { SaveAction, useSettingsContext } from './SettingsContext';
 import { Option, Select } from '../../lib/select/Select';
 import { SortVariantParamsForm, SortVariantSelect } from './SortVariantSettings';
+import { getVersion } from '@tauri-apps/api/app';
+import { useEffectOnce } from 'react-use';
+import * as Tooltip from '@radix-ui/react-tooltip';
+import { Info } from 'lucide-react';
 
 export function SettingsDialog({ children, ...props }: React.PropsWithChildren<Dialog.TriggerProps>) {
     const { settings, setSettings } = useSettingsContext();
+    const [version, setVersion] = React.useState<string | undefined>();
+
+    useEffectOnce(() => {
+        getVersion().then((data) => setVersion(data));
+    });
 
     return (
         <Dialog.Root>
             <Dialog.Trigger {...props}>{children}</Dialog.Trigger>
             <Dialog.Content className="flex flex-col gap-2">
-                <Dialog.Title>Einstellungen</Dialog.Title>
+                <div className="grid grid-cols-[40px_auto_40px]">
+                    <div></div>
+                    <Dialog.Title>Einstellungen</Dialog.Title>
+                    <Tooltip.Root delayDuration={300}>
+                        <Tooltip.Trigger asChild className="justify-self-end">
+                            <Button variant="ghost" tabIndex={-1}>
+                                <Info />
+                            </Button>
+                        </Tooltip.Trigger>
+                        <Tooltip.Content className="rounded-md border bg-popover p-1 shadow" sideOffset={4}>
+                            <div className="flex gap-2">
+                                <div className="flex flex-col">
+                                    <div className="font-bold text-xs">Version</div>
+                                    <div className="text-base">{version ?? '-'}</div>
+                                </div>
+                            </div>
+                        </Tooltip.Content>
+                    </Tooltip.Root>
+                </div>
                 <div className="flex justify-between gap-4 items-center">
                     <div>Speicheraktion</div>
                     <Select
